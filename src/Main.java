e1d9455aa8d6d008398cabb8147f8af8b09544a0
Owca420 <krystianwolosz2007@gmail.com>
2023-11-23 13:26:57 +0100

    wykonane

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ciąg znaków: ");
+        String ciag = scanner.nextLine();
+
+        int dlugosc = ciag.length() / 2;
+
+        String zamiana = ciag.indexOf();
+
+
+        String ciag2 =
+
+
+
+
+
+
+
 
     }
 }
